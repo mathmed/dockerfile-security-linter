0a8e15659a86603f8b289c47a1c2bb245c6846bb
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-25 12:22:09 -0300

    refactor: change to snake case

--- src/core/analysis/lexical/Token.py
+++ src/core/analysis/lexical/Token.py
@@ -1,7 +1,7 @@
 class Token:
-    def __init__(self, tokenTuple):
-        self.directive = tokenTuple[0]
-        self.original = tokenTuple[3]
-        self.start_line = tokenTuple[4]
-        self.end_line = tokenTuple[5]
-        self.value = tokenTuple[7]
+    def __init__(self, token_tuple):
+        self.directive = token_tuple[0]
+        self.original = token_tuple[3]
+        self.start_line = token_tuple[4]
+        self.end_line = token_tuple[5]
+        self.value = token_tuple[7]
