5fbcd6043cb0187634ccf8d1b6ba41b2c6e56427
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-25 14:05:20 -0300

    feat: add security smell sm02

--- src/core/rules/smells/smells.py
+++ src/core/rules/smells/smells.py
@@ -3,5 +3,10 @@ smells = {
     "smell": "Admin por padrão",
     "description": "Execução com privilégios desnecessários",
     "cwe": 250
+  },
+    "SM02": {
+    "smell": "Senha vazia",
+    "description": "Senha vazia no arquivo de configuração",
+    "cwe": 258
   }
 }
