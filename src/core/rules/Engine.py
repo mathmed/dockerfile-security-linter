02f2dbaa8179a0ae8ae74e9689c42a8ed6359263
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-25 11:51:14 -0300

    feat: add simple rules engine

---
+++ src/core/rules/Engine.py
@@ -0,0 +1,15 @@
+
+from .SM01 import SM01
+
+class Engine:
+
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.smells = []
+
+    def run(self):
+        for token in self.tokens:
+            sm01 = SM01(token).validade()
+            if(sm01):
+                self.smells.append(sm01)
+        return self.smells
