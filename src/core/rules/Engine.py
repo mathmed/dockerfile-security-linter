a31893ae9fb8dae9b22e94db871f2608a38c1117
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-25 14:00:54 -0300

    refactor: update engine

--- src/core/rules/Engine.py
+++ src/core/rules/Engine.py
@@ -1,5 +1,6 @@
 
 from .smells.SM01 import SM01
+from .smells.SM02 import SM02
 
 class Engine:
 
@@ -8,11 +9,13 @@ class Engine:
         self.smells = []
 
     def run(self):
+
         for token in self.tokens:
-            sm01 = self.validade(token)
+            sm01 = SM01(token).validade()
+            sm02 = SM02(token).validade()
             if(sm01):
                 self.smells.append(sm01)
+            if(sm02):
+                self.smells.append(sm02)
+                
         return self.smells
-
-    def validade(self, token):
-        return SM01(token).validade()
