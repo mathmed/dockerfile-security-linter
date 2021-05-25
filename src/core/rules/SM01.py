df14c028bb749a5e55ad8ea7f5ec9becf8942534
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-25 11:51:31 -0300

    feat: implement SM01 rule

---
+++ src/core/rules/SM01.py
@@ -0,0 +1,17 @@
+
+# Security Smell 01 - Admin por padrão - Execução com privilégios desnecessários (CWE-250)
+from .smells import *
+
+class SM01:
+    def __init__(self, token):
+        self.token = token
+    def validade(self):
+        if(self.token.directive == "user"):
+            if(self.token.value[0] == "root"):
+                return {
+                    "command": self.token.original, 
+                    "start_line": self.token.start_line, 
+                    "end_line": self.token.end_line, 
+                    "security_smell": smells["SM01"]
+                }
+        return False
