aa75a590301cc1bf9cd1c02c2a87a4d989139eef
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-25 14:15:43 -0300

    test: ensure sm02 return correct values

---
+++ tests/rules/smells/TestSM02.py
@@ -0,0 +1,23 @@
+import unittest
+from src.core.rules.smells.SM02 import SM02
+from src.core.rules.smells.smells import smells
+from src.core.analysis.lexical.Token import Token
+
+class TestSM02(unittest.TestCase):
+
+    def test_sm02_should_return_false(self):
+        sut = SM02(Token(["env", "any_sub", "any_json", "any_original", "any_start", "any_end", "any_flags", ["pass", "pass"]]))
+        self.assertEqual(sut.validade(), False)
+
+    def test_sm02_should_return_dict(self):
+        sut = SM02(Token(["env", "any_sub", "any_json", "any_original", "any_start", "any_end", "any_flags", ["pass", "''"]]))
+        expected = {
+                    "command": "env", 
+                    "start_line": "any_start", 
+                    "end_line": "any_end", 
+                    "security_smell": smells["SM02"]
+                }
+        self.assertEqual(sut.validade(), expected)
+        
+if __name__ == '__main__':
+    unittest.main()
