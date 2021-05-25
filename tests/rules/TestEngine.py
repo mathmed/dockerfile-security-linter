2e9adfe5b85b612d8aeb60f5568f0ec267190271
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-25 14:00:41 -0300

    test: update engine test

--- tests/rules/TestEngine.py
+++ tests/rules/TestEngine.py
@@ -2,13 +2,20 @@ import unittest
 from unittest.mock import MagicMock
 from src.core.analysis.lexical.Token import Token
 from src.core.rules.Engine import Engine
+from src.core.rules.smells.smells import smells
+
 
 class TestEngine(unittest.TestCase):
     
     def test_engine_should_return_valid_smells(self):
-        sut = Engine([Token(["any_cmd", "any_sub", "any_json", "any_original", "any_start", "any_end", "any_flags", "any_value"])])
-        sut.validade = MagicMock(return_value="any")
-        self.assertEqual(sut.run(), ["any"])
+        sut = Engine([Token(["user", "any_sub", "any_json", "any_original", "any_start", "any_end", "any_flags", ["root"]])])
+        expected = {
+                    "command": "user", 
+                    "start_line": "any_start", 
+                    "end_line": "any_end", 
+                    "security_smell": smells["SM01"]
+                }
+        self.assertEqual(sut.run(), [expected])
         
 if __name__ == '__main__':
     unittest.main()
