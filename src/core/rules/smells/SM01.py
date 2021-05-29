92a839c11267751f2759fa238a34a18872aeddd9
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-29 12:20:03 -0300

    refactor: create function to verify env directive

--- src/core/rules/smells/SM01.py
+++ src/core/rules/smells/SM01.py
@@ -3,15 +3,29 @@
 from .smells import *
 
 class SM01:
+
     def __init__(self, token):
         self.token = token
+
     def validade(self):
+
+        token = self.token
+        env_directive = self.verify_env_directive(token)
+
+        if(env_directive):
+            return env_directive
+
+        return False
+
+    def verify_env_directive(self, token):
+
         if(self.token.directive == "user"):
             if(self.token.value[0] == "root"):
                 return {
-                    "command": self.token.directive, 
+                    "command": self.token.original, 
                     "start_line": self.token.start_line, 
                     "end_line": self.token.end_line, 
                     "security_smell": smells["SM01"]
                 }
+                
         return False
