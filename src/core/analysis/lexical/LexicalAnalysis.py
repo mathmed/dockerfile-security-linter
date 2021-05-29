e0c2edd99e5ffd8533dea882a99d9d8979bd9623
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-29 13:02:45 -0300

    fix: chande variable name

--- src/core/analysis/lexical/LexicalAnalysis.py
+++ src/core/analysis/lexical/LexicalAnalysis.py
@@ -26,7 +26,7 @@ class LexicalAnalysis:
                 token[7]
             )
             if(token_obj.directive.lower() == "run"):
-                token_obj.set_value(self.run_shell_analysis(token.value[0]))
+                token_obj.set_value(self.run_shell_analysis(token_obj.value[0]))
             converted_tokens.append(token_obj)
         self.tokens = converted_tokens
 
