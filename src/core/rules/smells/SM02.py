df503d783f12d2b5c52f661ce5a5a3d402e92d57
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-05-29 12:21:14 -0300

    refactor: create function to verify env directive

--- src/core/rules/smells/SM02.py
+++ src/core/rules/smells/SM02.py
@@ -6,18 +6,27 @@ class SM02:
     def __init__(self, token):
         self.token = token
     def validade(self):
+
         token = self.token
+
+        env_directive = self.verify_env_directive(token)
+
+        if(env_directive):
+            return env_directive
+
+        return False
+
+    def verify_env_directive(self, token):
         if(token.directive == "env"):
             if(self.includes_pass(token.value[0])):
                 if(len(token.value) >= 1 and token.value[1].replace(" ", "") == "''"):
                     return {
-                        "command": token.directive, 
+                        "command": token.original, 
                         "start_line": token.start_line, 
                         "end_line": token.end_line, 
                         "security_smell": smells["SM02"]
                     }
-        return False
-
+            
     def includes_pass(self, string):
         return "pass" in string.lower() or "senha" in string.lower()
 
