2fe12211adf43f746e02bd7e9ce9e28dded75cb9
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-06-01 20:27:59 -0300

    refactor: add suspicious words

--- src/core/rules/smells/helpers/suspicious_words.py
+++ src/core/rules/smells/helpers/suspicious_words.py
@@ -2,8 +2,31 @@ suspicious_words = [
     "to do",
     "todo",
     "fix",
+    "fix me",
+    "fixme"
     "corrigir",
-    "bug",
+    " bug ",
     "not work",
-    "não funciona"
+    "não funciona",
+    "temporarily",
+    "temporário",
+    "temporario",
+    "issue",
+    "workaround",
+    "gambiarra",
+    "dont modify",
+    "don't modify",
+    "do not modify",
+    "do not edit",
+    "don't edit",
+    "dont edit",
+    "nao altere",
+    "não altere",
+    "não modifique"
+    "nao modifique",
+    "nao mexa"
+    "não mexa",
+    "atenção",
+    "atençao",
+    "warning"
 ]
