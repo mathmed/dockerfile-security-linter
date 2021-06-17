be1eee624cc8c8a0ac41dd9bc595b3e375458cc1
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-06-17 13:15:45 -0300

    refactor: ajust worker

--- worker.py
+++ worker.py
@@ -1,12 +1,17 @@
+# Worker para executar a análise em todos dockerfiles da base de dados
+
 import requests
 import json
 import glob
 from pathlib import Path
 from multiprocessing import Pool
 
+# URL da API e pasta onde os Dockerfiles estão
 url_api = "http://localhost:8000/dockerfile-analysis"
 dockerfiles_path = "/home/mateus/Área de Trabalho/TCC/TCC 2/dataset/datasets/0b-deduplicated-dockerfile-sources/sources/*.Dockerfile"
 
+
+# Converte um código de security smell para um índice de array
 def position(smell):
     smells = {
         "SM01": 0,
@@ -18,31 +23,54 @@ def position(smell):
         "SM07": 6,
         "SM08": 7,
         "SM09": 8,
-        "SM010": 9,
-        "SM011": 10,
+        "SM10": 9,
+        "SM11": 10,
     }
     return smells[smell]
 
+# Executa o worker
 def run(path):
 
-    smells = [0] * 12
+    # Cria o array com quantidade de smells, 11 smells, +1 posição para erros, +1 posição para N/A smell
+    smells = [0] * 13
+    
+    # Recebe o caminho do Dockerfile e cria o parâmetro para chamada da API
     path = str(path)
     params = {'dockerfile': Path(path).read_text()}
+
+    # Realiza a requisição a API e salva o valor retornado
     r = requests.post(url = url_api, json = params)
-    
-    try:
-        print("Verificando...")
-        smells[position(json.loads(r.text)["result"][0]["code"])] += 1
-    except:
-        smells[11] += 1
-        print("Dockerfile com sintaxe inválida.")
+    result = json.loads(r.text)["result"]
+
+    # Verifica se algum smell foi encontrado
+    if(result == "Nenhuma vulnerabilidade encontrada"):
+            smells[12] += 1
+    else:
+
+        # Salva os smells no array
+        try:
+            for item in result:
+                smells[position(item["code"])] += 1
+        except:
+            # Caso de erro na análise do Dockerfile
+            smells[11] += 1
+            print("Dockerfile com sintaxe inválida: ", path)
 
     return smells
     
+# Recupera todos os caminhos de Dockerfiles na pasta
 pathlist = glob.glob(dockerfiles_path)
+
+# Cria threads para realizar o processamento
 process = Pool(8)
-results = process.map(run, pathlist) 
-sumindex = [sum(elts) for elts in zip(*results)]
+
+# Realiza a chamada da função run com threads enviando os dockerfiles
+results = process.map(run, pathlist[:10000]) 
+
+# Soma todos os valores obtidos de smells na análise
+sumindex = [sum(item) for item in zip(*results)]
+
+# Exibe o resultado
 print(
     {
         "SM01": sumindex[0],
@@ -54,8 +82,9 @@ print(
         "SM07": sumindex[6],
         "SM08": sumindex[7],
         "SM09": sumindex[8],
-        "SM010": sumindex[9],
-        "SM011": sumindex[10],
+        "SM10": sumindex[9],
+        "SM11": sumindex[10],
         "Erros": sumindex[11],
+        "Nenhuma vulnerabilidade": sumindex[12],
     }
 )
